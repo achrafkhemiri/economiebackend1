bbdbc0562c9c55a7a98ebc3f3ddef03e83f5bfc0
achrafkhemiri <khemiriachraf2001@gmail.com>
2024-10-02 13:33:52 +0100

    Initial commit

---
+++ app/Http/Controllers/CategorieController.php
@@ -0,0 +1,86 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Categorie;
+use Illuminate\Http\Request;
+
+class CategorieController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     */
+    public function index()
+    {
+        try {
+            $categories=Categorie::all();
+            return response()->json($categories);
+            
+        } catch (\Exception $e) {
+            return response()->json("probleme de recuperation de la liste des categories");
+        }
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     */
+    public function store(Request $request)
+    {
+        try {
+            $categorie =new Categorie([
+                "nomcategorie"=>$request->input("nomcategorie"),
+                "imagecategorie"=>$request->input("imagecategorie")
+            ]);
+            $categorie->save();
+            return response()->json($categorie);
+            
+        } catch (\Exception $e) {
+            return response()->json("insertion impossible");
+        }
+    }
+
+    /**
+     * Display the specified resource.
+     */
+    public function show( $id)
+    {
+        try {
+            $categorie = Categorie::findOrfail($id);
+            return response()->json($categorie);
+            
+        } catch (\Exception $e) {
+            return response()->json("probleme de recuperation des donnees");
+        }
+    }
+
+    /**
+     * Update the specified resource in storage.
+     */
+    public function update(Request $request, Categorie $categorie,$id)
+    {
+        try {
+            $categorie = Categorie::findOrfail($id);
+            $categorie->update($request->all());
+            return response()->json($categorie  );
+            
+        } catch (\Exception $e) {
+            return response()->json("probleme modif");
+        }
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     */
+    public function destroy( $id)
+    {
+        try {
+            $categorie = Categorie::findOrfail($id);
+            $categorie->delete();
+            return response()->json("categorie supp ");
+            
+        } catch (\Exception $e) {
+            return response()->json("probleme de supp des donnees");
+        }
+        
+    }
+}
